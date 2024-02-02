93589ad28b4a16612d9d3f22e3d4f4deebdee6eb
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-02 17:04:26 +0300

    refactor the app to use express server for api

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,81 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
 
+// Set up an API using express
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./tanui-api-key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+// Initialize Cloud Firestore through Firebase
 const db = admin.firestore();
 
+// Test the root path of the API
+app.get('/', (req, res) => {
+    res.json({ message: "Hello from Firebase!" });
+}
+);
 
-exports.addInventory = functions.https.onRequest((req, res) => {
 
-  const inventory = req.body;
-  db.collection("inventory")
-    .add(inventory)
-    .then((ref) => {
-      res.send(ref.id);
-    });
-});
-
-exports.getInventory = functions.https.onRequest((req, res) => {
+// Get all inventory items
+app.get("/inventory", (req, res) => {
     db.collection("inventory")
         .get()
-        .then((snapshot) => {
-        let inventory = [];
-        snapshot.forEach((doc) => {
-            inventory.push({ id: doc.id, ...doc.data() });
+        .then((querySnapshot) => {
+
+         // Declare an empty array to store the inventory Item   
+            const inventory = [];
+            
+            // Loop through the data and store it in the inventory array
+        querySnapshot.forEach((doc) => {
+            inventory.push({
+            id: doc.id,
+            data: doc.data(),
+            });
         });
-        res.send(inventory);
-        });
-});
-    
-exports.updateInventory = functions.https.onRequest((req, res) => {
-    const id = req.query.id;
-    const
-        inventory = req.body;
-    db.collection("inventory")
-        .doc(id)
-        .update(inventory)
-        .then(() => {
-        res.send("Inventory updated");
+            // Return the inventory array
+        res.json(inventory);
         });
 }
 );
 
-exports.deleteInventory = functions.https.onRequest((req, res) => {
-    const id = req.query.id;
+// Add a new inventory item
+app.post("/inventory", (req, res) => {
+    const inventory = req.body;
+    db.collection("inventory")
+        .add(inventory)
+        .then(() => {
+        res.json("Inventory added successfully");
+        });
+}
+);
+
+// Update a specific inventory item
+app.put("/inventory/:id", (req, res) => {
+
+    // Extract the id from the request
+    const id = req.params.id;
+    const inventory = req.body;
+    db.collection("inventory")
+        .doc(id)
+        .update(inventory)
+        .then(() => {
+        res.send("Inventory updated successfully");
+        });
+}
+);
+
+// Delete a specific inventory item
+app.delete("/inventory/:id", (req, res) => {
+    const id = req.params.id;
     db.collection("inventory")
         .doc(id)
         .delete()
@@ -51,13 +85,24 @@ exports.deleteInventory = functions.https.onRequest((req, res) => {
 }
 );
 
-exports.getInventoryById = functions.https.onRequest((req, res) => {
-    const id = req.query.id;
+// Get a specific inventory item by id
+app.get("/inventory/:id", (req, res) => {
+    const id = req.params.id;
     db.collection("inventory")
         .doc(id)
         .get()
         .then((doc) => {
-        res.send(doc.data());
+        if (!doc.exists) {
+            res.send("No such document");
+        } else {
+            res.json({
+            id: doc.id,
+            data: doc.data(),
+            });
+        }
         });
 }
 );
+
+// Export the express app as an HTTP Cloud Function
+exports.app = functions.https.onRequest(app);
